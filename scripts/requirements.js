bae5712307c123ad47d4379fc1f9a6b68c8c1dc8
Frostpink <missfussy24@outlook.com>
2023-02-05 17:47:35 -0500

    fix check styling for vegetarian and glutenfree

--- scripts/requirements.js
+++ scripts/requirements.js
@@ -8,6 +8,8 @@ var requirements = {
 const getRequirements = () => {
   var data = document.getElementsByClassName("requirement1");
   for (const element of data) {
+    const labelElement = document.querySelector(".products-filters").querySelector(`label[for="${element.id}"]`);
+    ((element.checked) ? labelElement.setAttribute("data-selected", "true") : labelElement.removeAttribute("data-selected"));
     var name = element.name;
     requirements[name] = element.checked;
   }
